18e12133138c2649fa14894fbf4cdea888464bc9
krasilnikow <andrey.krasilnikov@clevercraft.net>
2026-07-12 19:16:41 +0300

    Правлю prod билд

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  build: {
+    minify: false,
+    sourcemap: true,
+  },
   plugins: [react()],
 })
