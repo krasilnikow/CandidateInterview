cb74a95e7fb807f0afb17fb9839c5a5419acea9f
as.krasilnikov <as.krasilnikov@tensor.ru>
2026-07-17 10:30:06 +0300

    Тест нагрузки

--- src/tasks/task2/components/SyncProcessor.jsx
+++ src/tasks/task2/components/SyncProcessor.jsx
@@ -5,7 +5,7 @@ const SyncProcessor = ({ value }) => {
     if (value) {
       const start = performance.now();
       let i = 0;
-      while (performance.now() - start < 1000) {
+      while (performance.now() - start < 2000) {
         for (let j = 0; j < 1000000; j++) {
           i = (i + j) * 31 % 100000;
         }
